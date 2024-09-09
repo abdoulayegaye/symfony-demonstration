b0a3d6c0a17b65c68d036bbfe6cf8795702abe77
Abdoulaye GAYE [SNT DSI/DAC/DIM/DS] <tmp_gaye74708@orange-sonatel.com>
2024-09-09 21:04:35 +0000

    third commit

---
+++ src/Controller/SigninController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\SigninType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SigninController extends AbstractController
+{
+    #[Route('/signin', name: 'app_signin')]
+    public function index(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(SigninType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('signin/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
